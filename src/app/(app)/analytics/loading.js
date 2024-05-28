22da08affd91c53c2ea33de8adb7b81b5c93f87e
Calvin Ucheoma <calvinchukwuma@gmail.com>
2024-05-28 22:45:50 +0100

    finished project

---
+++ src/app/(app)/analytics/loading.js
@@ -0,0 +1,11 @@
+import '../../globals.css';
+
+const Loading = () => {
+  return (
+    <div className="fixed w-full h-screen top-0 left-0 grid place-items-center">
+      <span className="loader"></span>
+    </div>
+  );
+};
+
+export default Loading;
--- src/app/(app)/analytics/page.js
+++ src/app/(app)/analytics/page.js
@@ -10,6 +10,8 @@ import mongoose from 'mongoose';
 import { getServerSession } from 'next-auth';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
+import { Suspense } from 'react';
+import Loading from './loading';
 
 const AnalyticsPage = async () => {
   await mongoose.connect(process.env.MONGODB_URI);
@@ -37,7 +39,7 @@ const AnalyticsPage = async () => {
   // });
 
   const views = await Event.find({
-    page: page.uri,
+    page: page?.uri,
     type: 'view',
   });
 
@@ -71,93 +73,95 @@ const AnalyticsPage = async () => {
     : []; // we use minus '-' sign to indicate descending order
 
   const clicks = await Event.find({
-    page: page.uri,
+    page: page?.uri,
     type: 'click',
   });
 
   return (
-    <div>
-      <SectionBox>
-        <h2 className="text-xl mb-6 text-center">Views</h2>
-        {/* Views: {viewsCount} <br />
+    <Suspense fallback={<Loading />}>
+      <div>
+        <SectionBox>
+          <h2 className="text-xl mb-6 text-center">Views</h2>
+          {/* Views: {viewsCount} <br />
         Clicks: {clicksCount} */}
-        {/* {groupedViews.map(({ _id: date, count }, index) => (
+          {/* {groupedViews.map(({ _id: date, count }, index) => (
           <div key={index}>
             {date}: {count}
           </div>
         ))} */}
-        {views ? (
-          <Chart
-            data={groupedViews?.map((object) => ({
-              date: object._id,
-              views: object.count,
-            }))}
-          />
-        ) : (
-          <h3 className="text-2xl text-center h-full flex items-center justify-center">
-            You do not have any views yet...
-          </h3>
-        )}
-      </SectionBox>
+          {views ? (
+            <Chart
+              data={groupedViews?.map((object) => ({
+                date: object._id,
+                views: object.count,
+              }))}
+            />
+          ) : (
+            <h3 className="text-2xl text-center h-full flex items-center justify-center">
+              You do not have any views yet...
+            </h3>
+          )}
+        </SectionBox>
 
-      <SectionBox>
-        <h2 className="text-xl mb-6 text-center">Clicks</h2>
+        <SectionBox>
+          <h2 className="text-xl mb-6 text-center">Clicks</h2>
 
-        {page?.links?.map((link) => (
-          <div
-            key={link.key}
-            className="md:flex gap-4 items-center border-t border-gray-200 py-4"
-          >
-            <div className="text-blue-500 pl-0 md:pl-4">
-              <FontAwesomeIcon icon={faLink} className="w-5 h-5" />
-            </div>
+          {page?.links?.map((link) => (
+            <div
+              key={link.key}
+              className="md:flex gap-4 items-center border-t border-gray-200 py-4"
+            >
+              <div className="text-blue-500 pl-0 md:pl-4">
+                <FontAwesomeIcon icon={faLink} className="w-5 h-5" />
+              </div>
 
-            <div className="grow w-full md:w-[60%]">
-              <h3>{link.title || 'No title'}</h3>
-              <p className="text-gray-700 text-sm">
-                {link.subtitle || 'No description'}
-              </p>
-              <Link
-                href={link.url}
-                target="_blank"
-                className="text-xs text-blue-400"
-              >
-                {link.url}
-              </Link>
-            </div>
+              <div className="grow w-full md:w-[60%]">
+                <h3>{link.title || 'No title'}</h3>
+                <p className="text-gray-700 text-sm">
+                  {link.subtitle || 'No description'}
+                </p>
+                <Link
+                  href={link.url}
+                  target="_blank"
+                  className="text-xs text-blue-400"
+                >
+                  {link.url}
+                </Link>
+              </div>
 
-            <div className="text-center">
-              <div className="border-2 border-rounded-md p-2 mt-1 md:mt-0">
-                <div className="text-3xl">
-                  {
-                    clicks?.filter(
-                      (click) =>
-                        click.uri === link.url && isToday(click.createdAt)
-                    ).length
-                  }
+              <div className="text-center">
+                <div className="border-2 border-rounded-md p-2 mt-1 md:mt-0">
+                  <div className="text-3xl">
+                    {
+                      clicks?.filter(
+                        (click) =>
+                          click.uri === link.url && isToday(click.createdAt)
+                      ).length
+                    }
+                  </div>
+
+                  <div className="text-gray-400 text-xs uppercase font-bold ">
+                    Clicks Today
+                  </div>
                 </div>
+              </div>
 
-                <div className="text-gray-400 text-xs uppercase font-bold ">
-                  Clicks Today
+              <div className="text-center">
+                <div className="border-2 border-rounded-md p-2 mt-1 md:mt-0">
+                  <div className="text-3xl">
+                    {clicks?.filter((click) => click.uri === link.url).length}
+                  </div>
+
+                  <div className="text-gray-400 text-xs uppercase font-bold ">
+                    Total Clicks
+                  </div>
                 </div>
               </div>
             </div>
-
-            <div className="text-center">
-              <div className="border-2 border-rounded-md p-2 mt-1 md:mt-0">
-                <div className="text-3xl">
-                  {clicks?.filter((click) => click.uri === link.url).length}
-                </div>
-
-                <div className="text-gray-400 text-xs uppercase font-bold ">
-                  Total Clicks
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </SectionBox>
-    </div>
+          ))}
+        </SectionBox>
+      </div>
+    </Suspense>
   );
 };
 
