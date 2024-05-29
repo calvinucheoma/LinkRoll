ed8e48d1ec3e4039b2b314368530501b5c65b171
Calvin Ucheoma <calvinchukwuma@gmail.com>
2024-05-29 16:03:42 +0100

    finished project

--- src/app/(app)/analytics/page.js
+++ src/app/(app)/analytics/page.js
@@ -32,170 +32,111 @@ const AnalyticsPage = async () => {
     type: 'view',
   });
 
-  if (!views || views.length === 0) {
-    return (
-      <Suspense fallback={<Loading />}>
-        <div>
-          <SectionBox>
-            <h3 className="text-2xl text-center h-full flex items-center justify-center">
-              You do not have any views yet...
-            </h3>
-          </SectionBox>
-          <SectionBox>
-            <h2 className="text-xl mb-6 text-center">Clicks</h2>
-            {page?.links?.length === 0 ? (
-              <h3 className="text-2xl text-center h-full flex items-center justify-center">
-                You do not have any links yet...
-              </h3>
-            ) : (
-              page.links.map((link) => (
-                <div
-                  key={link.key}
-                  className="md:flex gap-4 items-center border-t border-gray-200 py-4"
-                >
-                  <div className="text-blue-500 pl-0 md:pl-4">
-                    <FontAwesomeIcon icon={faLink} className="w-5 h-5" />
-                  </div>
-                  <div className="grow w-full md:w-[60%]">
-                    <h3>{link.title || 'No title'}</h3>
-                    <p className="text-gray-700 text-sm">
-                      {link.subtitle || 'No description'}
-                    </p>
-                    <Link
-                      href={link.url}
-                      target="_blank"
-                      className="text-xs text-blue-400"
-                    >
-                      {link.url}
-                    </Link>
-                  </div>
-                  <div className="text-center">
-                    <div className="border-2 border-rounded-md p-2 mt-1 md:mt-0">
-                      <div className="text-3xl">
-                        {
-                          clicks.filter(
-                            (click) =>
-                              click.uri === link.url && isToday(click.createdAt)
-                          ).length
-                        }
-                      </div>
-                      <div className="text-gray-400 text-xs uppercase font-bold ">
-                        Clicks Today
-                      </div>
-                    </div>
-                  </div>
-                  <div className="text-center">
-                    <div className="border-2 border-rounded-md p-2 mt-1 md:mt-0">
-                      <div className="text-3xl">
-                        {
-                          clicks.filter((click) => click.uri === link.url)
-                            .length
-                        }
-                      </div>
-                      <div className="text-gray-400 text-xs uppercase font-bold ">
-                        Total Clicks
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))
-            )}
-          </SectionBox>
-        </div>
-      </Suspense>
-    );
-  }
-
-  const groupedViews = await Event.aggregate([
-    {
-      $match: {
-        type: 'view',
-        uri: page?.uri,
-      },
-    },
-    {
-      $group: {
-        _id: {
-          $dateToString: {
-            date: '$createdAt',
-            format: '%Y-%m-%d',
-          },
-        },
-        count: {
-          $count: {},
-        },
-      },
-    },
-    { $sort: { _id: 1 } },
-  ]);
-
   const clicks = await Event.find({
     page: page?.uri,
     type: 'click',
   });
+
+  const groupedViews =
+    views.length > 0
+      ? await Event.aggregate([
+          {
+            $match: {
+              type: 'view',
+              uri: page?.uri,
+            },
+          },
+          {
+            $group: {
+              _id: {
+                $dateToString: {
+                  date: '$createdAt',
+                  format: '%Y-%m-%d',
+                },
+              },
+              count: {
+                $count: {},
+              },
+            },
+          },
+          { $sort: { _id: 1 } },
+        ])
+      : [];
 
   return (
     <Suspense fallback={<Loading />}>
       <div>
         <SectionBox>
           <h2 className="text-xl mb-6 text-center">Views</h2>
-          <Chart
-            data={groupedViews.map((object) => ({
-              date: object._id,
-              views: object.count,
-            }))}
-          />
+          {views.length > 0 ? (
+            <Chart
+              data={groupedViews.map((object) => ({
+                date: object._id,
+                views: object.count,
+              }))}
+            />
+          ) : (
+            <h3 className="text-2xl text-center h-full flex items-center justify-center">
+              You do not have any views yet...
+            </h3>
+          )}
         </SectionBox>
         <SectionBox>
           <h2 className="text-xl mb-6 text-center">Clicks</h2>
-          {page?.links?.map((link) => (
-            <div
-              key={link.key}
-              className="md:flex gap-4 items-center border-t border-gray-200 py-4"
-            >
-              <div className="text-blue-500 pl-0 md:pl-4">
-                <FontAwesomeIcon icon={faLink} className="w-5 h-5" />
-              </div>
-              <div className="grow w-full md:w-[60%]">
-                <h3>{link.title || 'No title'}</h3>
-                <p className="text-gray-700 text-sm">
-                  {link.subtitle || 'No description'}
-                </p>
-                <Link
-                  href={link.url}
-                  target="_blank"
-                  className="text-xs text-blue-400"
-                >
-                  {link.url}
-                </Link>
-              </div>
-              <div className="text-center">
-                <div className="border-2 border-rounded-md p-2 mt-1 md:mt-0">
-                  <div className="text-3xl">
-                    {
-                      clicks.filter(
-                        (click) =>
-                          click.uri === link.url && isToday(click.createdAt)
-                      ).length
-                    }
+          {page.links.length > 0 ? (
+            page.links.map((link) => (
+              <div
+                key={link.key}
+                className="md:flex gap-4 items-center border-t border-gray-200 py-4"
+              >
+                <div className="text-blue-500 pl-0 md:pl-4">
+                  <FontAwesomeIcon icon={faLink} className="w-5 h-5" />
+                </div>
+                <div className="grow w-full md:w-[60%]">
+                  <h3>{link.title || 'No title'}</h3>
+                  <p className="text-gray-700 text-sm">
+                    {link.subtitle || 'No description'}
+                  </p>
+                  <Link
+                    href={link.url}
+                    target="_blank"
+                    className="text-xs text-blue-400"
+                  >
+                    {link.url}
+                  </Link>
+                </div>
+                <div className="text-center">
+                  <div className="border-2 border-rounded-md p-2 mt-1 md:mt-0">
+                    <div className="text-3xl">
+                      {
+                        clicks.filter(
+                          (click) =>
+                            click.uri === link.url && isToday(click.createdAt)
+                        ).length
+                      }
+                    </div>
+                    <div className="text-gray-400 text-xs uppercase font-bold ">
+                      Clicks Today
+                    </div>
                   </div>
-                  <div className="text-gray-400 text-xs uppercase font-bold ">
-                    Clicks Today
+                </div>
+                <div className="text-center">
+                  <div className="border-2 border-rounded-md p-2 mt-1 md:mt-0">
+                    <div className="text-3xl">
+                      {clicks.filter((click) => click.uri === link.url).length}
+                    </div>
+                    <div className="text-gray-400 text-xs uppercase font-bold ">
+                      Total Clicks
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="text-center">
-                <div className="border-2 border-rounded-md p-2 mt-1 md:mt-0">
-                  <div className="text-3xl">
-                    {clicks.filter((click) => click.uri === link.url).length}
-                  </div>
-                  <div className="text-gray-400 text-xs uppercase font-bold ">
-                    Total Clicks
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            <h3 className="text-2xl text-center h-full flex items-center justify-center">
+              You do not have any links yet...
+            </h3>
+          )}
         </SectionBox>
       </div>
     </Suspense>
